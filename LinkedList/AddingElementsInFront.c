18b9c1e7608766750f3477537a00b585da9f3266
“subhradeepdutta” <subhradeep.dutta@colorado.edu>
2016-12-31 03:07:47 -0700

    Updated Linked List program for insertion at Nth position

--- LinkedList/AddingElementsInFront.c
+++ LinkedList/AddingElementsInFront.c
@@ -12,7 +12,8 @@
 
 
 void PrintElements();
-void InsertElement(int x);
+void InsertElementInTheBeginning(int x);
+void InsertElementAtN(int x, int n);
 
 struct node
 {
@@ -23,6 +24,8 @@ struct node
 struct node* HEAD;
 struct node* temp;
 struct node* temp1;
+struct node* temp2;
+struct node* temp3;
 
 
 /*************************************************************************************
@@ -54,13 +57,42 @@ Input         : Element to be inserted
 Output        : N/A
 *************************************************************************************/
 
-void InsertElement(int x)
+void InsertElementInTheBeginning(int x)
 {
     struct node* temp=(struct node*)malloc(sizeof(struct node));
     temp->data=x;
     temp->next=HEAD;
     HEAD=temp;
 
+}
+
+/*************************************************************************************
+Description   : This function is used to insert elements in the nth position of the
+                linked list
+Input         : Element to be inserted
+Output        : N/A
+*************************************************************************************/
+
+void InsertElementAtN(int x, int n)
+{
+    int i;
+    struct node* temp2=(struct node*)malloc(sizeof(struct node));
+    temp2->data=x;
+    temp2->next=NULL;
+    if(n==1)
+    {
+        temp2->next=HEAD;
+        HEAD=temp2;
+        return;
+    }
+    struct node* temp3=HEAD;
+    for(i=0;i<n-2;i++)
+    {
+        printf("Inside the loop");
+        temp3=temp3->next;
+    }
+    temp2->next=temp3->next;
+    temp3->next=temp2;
 }
 
 int main()
@@ -75,10 +107,13 @@ int main()
     {
         printf("\n\rEnter the number");
         scanf("%d",&x);
-        InsertElement(x);
+        InsertElementInTheBeginning(x);
         PrintElements();
     }
-
+        printf("\n\rEnter the number and position");
+        scanf("%d %d",&x,&n);
+        InsertElementAtN(x, n);
+        PrintElements();
     return 0;
 }
 
